531c62b8f6c032d25ab21a4281cce4ebbecac590
Carlos Moraes <carloshrmoraes@gmail.com>
2023-12-05 08:31:41 -0300

    add timer for benchmarking

--- advent2023.cpp
+++ advent2023.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include "Day4.h"
+#include <chrono>
 
 int main()
 {
     Day4 solution{ };
+    auto start = std::chrono::high_resolution_clock::now();
     std::cout << solution.partOne() << "||" << solution.partTwo();
+    auto end = std::chrono::high_resolution_clock::now();
+
+    auto time = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+    std::cout << "\n run time: " << time.count() << "ms" << std::endl;
 }
